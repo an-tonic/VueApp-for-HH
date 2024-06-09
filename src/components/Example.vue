3400810bcd110c9ad13a09f436639fdd093758e2
serdy <aserdyukov@uclan.ac.uk>
2024-06-09 13:40:23 +0300

    Initial Commit

---
+++ src/components/Example.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+const increment = () => {
+  count.value++;
+};
+</script>
+
+<style scoped>
+button {
+  padding: 5px 10px;
+  font-size: 16px;
+}
+</style>
