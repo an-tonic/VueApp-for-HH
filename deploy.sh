f73b4dae2501807549c5ab9546e1cdbc6b0f4098
serdy <aserdyukov@uclan.ac.uk>
2024-06-09 14:57:39 +0300

    Test

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:an-tonis/hh-test-rick-and-morty-characters.git master:gh-pages
+git push -f git@github.com:an-tonic/hh-test-rick-and-morty-characters.git master:gh-pages
 cd -
